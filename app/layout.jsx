dd10b65ea7d63f41df5b545082ebb16c56e670f5
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-12 16:47:54 +0300

    feat: add nav component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Nav from '@components/Nav';
 import '@styles/global.css';
 
 export const metadate = {
@@ -13,6 +14,7 @@ const RootLayout = ({ children }) => {
                 <div className="gradient" />
             </div>
             <main className="app">
+            <Nav />
                 {children}
             </main>
         </body>
