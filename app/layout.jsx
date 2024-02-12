d9eddf3cd2f0f10d18abb527b48d63ad6ee936f9
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-12 19:08:32 +0300

    feat: add Provider component to RootLayout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,28 @@
-import Nav from '@components/Nav';
-import '@styles/global.css';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/global.css";
 
 export const metadate = {
-    title: "Prompt Collab",
-    description: "Discover & Share AI Prompts"
-}
+  title: "Prompt Collab",
+  description: "Discover & Share AI Prompts",
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
-            <main className="app">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
             <Nav />
-                {children}
-            </main>
-        </body>
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
