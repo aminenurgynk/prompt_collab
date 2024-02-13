129cec505348d10e736e47580f48b1bce6f5a1b4
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-13 06:18:07 +0300

    feat: add user session and sign-in functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
-
+import User from '@models/user';
 import { connectToDB } from "@utils/database";
 
 
@@ -12,7 +12,12 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+        return session;
     },
     async signIn({profile}) {
         try {
@@ -20,8 +25,17 @@ const handler = NextAuth({
             await connectToDB();
 
             // check if user already exists in database
-
+            const userExists = await User.findOne({
+                email: profile.email
+            });
             // if not, create a new user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(' ', '').toLowerCase(),
+                    image: profile.picture
+                })
+            }
 
             return true;
         } catch (error) {
